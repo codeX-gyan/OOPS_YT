aff85b733f963b2108cf89a9d4d25b483a8245c7
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-10 23:42:49 +0100

    Initial commit

---
+++ UserDefinedDatatypes.cpp
@@ -0,0 +1,14 @@
+#include<iostram>
+using namespace std;
+class Student() {
+    string name;
+    float rno;
+    float cgpa;
+
+}
+int main() {
+    Student s;
+    s.name = 'gyan';
+    s.rno = 78;
+    s.cgpa = 8.0
+}
