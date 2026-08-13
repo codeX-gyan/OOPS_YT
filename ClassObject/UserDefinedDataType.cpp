d40cba33645afb3c5f2528f8e81e2afc94728b3c
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-13 20:16:31 +0100

    Update UserDefinedDataType.cpp

--- ClassObject/UserDefinedDataType.cpp
+++ ClassObject/UserDefinedDataType.cpp
@@ -9,16 +9,19 @@ public:
 };
 int main() {
     Student s1;
-    s1.name = "gyan";
-    s1.rno = 78;
+   s1.name = "gyan";
+    //s1.rno = 78;
+    cout<<"enter roll : ";
+    cin>>s1.rno;
+
     s1.cgpa = 8.0;
 
-    Student s2;
-    s2.name = "prakash";
-    s2.rno = 80;
-    s2.cgpa = 8.5;
+    // Student s2;
+    // s2.name = "prakash";
+    // s2.rno = 80;
+    // s2.cgpa = 8.5;
 
     cout<<s1.name<<" "<<s1.cgpa<<" "<<s1.rno<<" "<<endl;
-    cout<<s2.name<<" "<<s2.cgpa<<" "<<s2.rno<<" "<<endl;
+    //cout<<s2.name<<" "<<s2.cgpa<<" "<<s2.rno<<" "<<endl;
 
 }
