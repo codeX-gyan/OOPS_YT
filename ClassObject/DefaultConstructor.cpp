5e289da655d49efd95f06393feda667d933ed675
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-18 11:06:17 +0100

    various constructors

--- ClassObject/DefaultConstructor.cpp
+++ ClassObject/DefaultConstructor.cpp
@@ -8,6 +8,11 @@ public:
     int seat;
     string type;
 
+    //----DEFAULT CONSTRUCTOR----
+    Car() {
+
+    }
+
     //----PARAMETERISED CONSTRUCTOR----
     Car( string n , int p , int s, string t){
         name = n ;
@@ -22,16 +27,22 @@ int main() {
 
     // c1.name ="Toyota";
     // c1.price = 4500000;
-    // c1.seat = 8;
-    // c1.type = "SUV";
+        //-----OVERWRITE OR NOT ?? -----
+    c1.seat = 6;                            
+    c1.type = "SUVplus";
 
-    Car c2( "Thar" , 1750000 , 4 , "JEEP");
-    // c2.name ="Thar";
-    // c2.price = 1750000;
-    // c2.seat = 4;
-    // c2.type = "JEEP";
+    //Car c2( "Thar" , 1750000 , 4 , "JEEP");
+    Car c2;
+    c2.name ="Thar";
+    c2.price = 1750000;
+    c2.seat = 4;
+    c2.type = "JEEP";
 
     cout<<c1.name<<" "<<c1.type<<" "<<c1.price<<" "<<c1.seat<<" "<<endl;
     cout<<c2.name<<" "<<c2.type<<" "<<c2.price<<" "<<c2.seat<<" "<<endl;
 
 }
+
+
+
+//CONSTRUCTORS ME HUM CHAHE TOH KAM VALUE BHI PRINT KARA SKTE H IMP NHI H KI JITNE BHI ATTRIBUTES HAI WO SARE PRINT KARAYE 
---
+++ ClassObject/MultipleConstructor.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class Student {
+public:    
+    string name;
+    float rno;
+    float cgpa;
+    int bc;
+
+    Student(string n , float r , float c , int b) {
+        name = n;
+        rno = r;
+        cgpa = c;
+        bc = b;
+    }
+
+    Student() {
+
+    }
+
+    Student( float r , string n){
+        name = n ;
+        rno = r;
+    }
+
+    Student(float r){
+        rno = r;
+    }
+};
+int main() {
+    Student s1("gyan",32,8.0,0);
+    //s1.name = "gyan";
+    //s1.rno = 78;
+    //cout<<"enter roll : ";
+    //cin>>s1.rno;
+
+    //s1.cgpa = 8.0;
+    //s1.bc = 1;
+
+    Student s2;
+    s2.name = "prakash";
+    s2.rno = 80;
+    s2.cgpa = 8.5;
+
+    Student s3( 65 );
+
+    Student s4( 33 , "gaurav");
+
+
+    cout<<s1.name<<" "<<s1.cgpa<<" "<<s1.rno<<" "<<s1.bc<<" "<<endl;
+    cout<<s2.name<<" "<<s2.cgpa<<" "<<s2.rno<<" "<<endl;
+    cout<<s3.rno<<" "<<endl;
+    cout<<s4.name<<" "<<s4.rno<<endl;
+
+}
