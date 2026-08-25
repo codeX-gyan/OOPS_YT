59f6a76480f52ed3a3e2a6831c0e5d303e10f2f0
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-25 11:36:02 +0100

    Create object.cpp

---
+++ ObjectPointer/object.cpp
@@ -0,0 +1,11 @@
+//----POINTERS RECAP-----
+#include<iostream>
+using namespace std;
+int main() {
+    int x = 4;
+    cout<<x<<endl;        // x ki val print hogi
+    cout<<&x<<endl;       // x ki address print hogi
+    int* p = &x;          // p me x ki address store hogyi
+    cout<<p<<endl;        // p ke ander jo address store hui thi wo adrs print hogi
+    cout<<*p<<endl;       // p ke andr wali adrs ki value print hogi i.e X ki val
+}
